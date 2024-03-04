a17401aa5d12e63651bfe5cc266f3e7265b65ac1
totylkopierdola <kudraj7@wp.pl>
2024-03-04 16:48:08 +0100

    player info and search player in progress

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="h-full min-h-screen flex flex-col ">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Play:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="h-full min-h-screen flex flex-col  ">
         <Main />
         <NextScript />
       </body>
