f008ff1bb40056556c01aeea6f4f3deaba3b21c8
totylkopierdola <kudraj7@wp.pl>
2024-03-07 16:55:57 +0100

    playerData.latestMatches done

--- src/pages/player-info/[nickname].tsx
+++ src/pages/player-info/[nickname].tsx
@@ -5,18 +5,35 @@ import { usePlayerData } from "../hooks/PlayerDataContext";
 const PlayerInfo = () => {
   const router = useRouter();
   const { nickname } = router.query;
-  const { playerData, getPlayerData } = usePlayerData();
+  const { playerData, getPlayerData, getPlayerLatestMatches } = usePlayerData();
 
   useEffect(() => {
     if (nickname !== undefined) {
       getPlayerData(nickname);
+      getPlayerLatestMatches(playerData.id, 2);
     }
-  }, [nickname]);
+  }, [nickname, playerData.id]);
 
   return (
     <div className="w-full border flex flex-col container items-center">
+      <button
+        onClick={() => console.log("playerData.id", playerData.id)}
+        className="border border-red-500"
+      >
+        playerData.id
+      </button>
+      <button
+        onClick={() =>
+          console.log(
+            "playerData.latestMatches.items",
+            playerData.latestMatches.items
+          )
+        }
+        className="border border-green-500"
+      >
+        playerData.latestMatches.items
+      </button>
       <div className="flex items-center flex-col border-2 w-2/3 border-green-600 ">
-        <h1>GENERAL</h1>
         <h2>{playerData.info.nickname}</h2>
         <h4>player stats </h4>
         <button className="btn-primary primary button border bg-green-400 rounded px-4">
@@ -69,11 +86,11 @@ const PlayerInfo = () => {
           </div>
         </div>
       </div>
-      {/* <div className="flex justfy-center flex -col w-2/3">
-        <h2>MATCHES</h2>
-        {playerStats.items &&
-          playerStats.items.map((match, index) => (
+      <div className="container border-yellow-500 border flex flex-col">
+        {playerData.latestMatches.items &&
+          playerData.latestMatches.items.map((match, index) => (
             <div key={index} className="flex flex-col  my-4 rounded-full">
+              <h2>MATCH {index + 1}:</h2>
               {Object.entries(match.stats).map(([statName, statValue]) => (
                 <div key={statName} className="flex w-full ">
                   <p className="font-bold w-40 px-4 bg-gray-400 ">
@@ -86,7 +103,7 @@ const PlayerInfo = () => {
               ))}
             </div>
           ))}
-      </div> */}
+      </div>
     </div>
   );
 };
